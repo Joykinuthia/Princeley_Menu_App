3bc68edcb1db23a2cb3a22de0630be256bb84148
Joykinuthia <joyrosekinuthia@gmail.com>
2025-04-02 22:22:36 +0300

    filtering event listener

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",() => {
     const cartContainer = document.getElementById("cart-items");
     const totalElement = document.getElementById("total");
     const orderButton = document.getElementById("order-btn");
-    const filterButton = {
+    const filterButtons = {
         all: document.getElementById("all-btn"),
         appetizer: document.getElementById("appetizer-btn"),
         main: document.getElementById("main-btn"),
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded",() => {
             <h3> ${item.name}</h3>
             <p> ${item.description}</p>
             <p class="price">$${item.price.toFixed(2)}</p>
-            <button class = "addToCart" data-id= "${item.id}">Add to Cart</button`;
+            <button class = "add-to-cart" data-id= "${item.id}">Add to Cart</button`;
 
             menuContainer.appendChild(menuItem);
         });
@@ -54,7 +54,33 @@ document.addEventListener("DOMContentLoaded",() => {
             cartContainer.appendChild(cartItem);
         })
 
-        total
+        totalElement.textContent = total.toFixed(2);
     }
 
+    //Adding items to cart
+    menuContainer.addEventListener("click",(event) => {
+        if(event.target.classList.contains("add-to-cart")) {
+            const itemId = parseInt(event.target.getAttribute("data-id"));
+            const selectedItem = menuData.find(item => item.id === itemId);
+            if (selectedItem) {
+                cart.push(selectedItem);
+                updateCart();
+            }
+        }
+    })
+
+    // filtering Event listeners
+
+    Object.keys(filterButtons).forEach(category => {
+        filterButtons[category].addEventListener("click", () => {
+            if (category === "all") {
+                displayMenu(menuData);
+            }else{
+                const filteredItems = menuData.filter(item => item.category === category);
+                displayMenu(filteredItems);
+            }
+        })
+    })
+
+    //place order
 })
