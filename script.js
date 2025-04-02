0e514d54037a3281de6c8d711e9221810dd28277
Joykinuthia <joyrosekinuthia@gmail.com>
2025-04-02 20:16:53 +0300

    Initializing key elements

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-document.getElementById("DOMContentLoaded",() => {
+document.addEventListener("DOMContentLoaded",() => {
     const menuContainer = document.getElementById("menu-items");
     const cartContainer = document.getElementById("cart-items");
     const totalElement = document.getElementById("total");
     const orderButton = document.getElementById("order-btn");
-    
+    const filterButton = {
+        all: document.getElementById("all-btn"),
+        appetizer: document.getElementById("appetizer-btn"),
+        main: document.getElementById("main-btn"),
+        dessert: document.getElementById("dessert-btn")
+    };
+
+    let menuData = [];
+    let cart = [];
+
 })
