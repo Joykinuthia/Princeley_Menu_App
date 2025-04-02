99f0dc8e92ca52f168420e17dbbb274fb0b14068
Joykinuthia <joyrosekinuthia@gmail.com>
2025-04-02 19:56:06 +0300

    initialcommit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("DOMContentLoaded",() => {
+    const menuContainer = document.getElementById("menu-items");
+    const cartContainer = document.getElementById("cart-items");
+    const totalElement = document.getElementById("total");
+    const orderButton = document.getElementById("order-btn");
+    
+})
